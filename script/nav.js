56b956f1c9ac5a9e964d855d0a10489f074afe1c
ansu-kri <ansukummari@gmail.com>
2023-01-01 00:34:53 -0800

    add

--- script/nav.js
+++ script/nav.js
@@ -2,5 +2,5 @@ import { Navbar } from "../components/navbar.js";
 document.getElementById("navbars").innerHTML=Navbar();
 
 
-import {Footer} from "../components/navbar.js"
+import {Footer} from "../components/footer.js"
 document.getElementById("footer").innerHTML=Footer();
